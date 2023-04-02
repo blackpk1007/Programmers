3b5ad8b50bfa685cf68356b34b7d0ea4c7ff1964
blackpk1007 <34873340+blackpk1007@users.noreply.github.com>
2023-04-03 01:09:52 +0900

    [level 0] Title: 자릿수 더하기, Time: 5.87 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str= n+"";
+        
+        for(int i=0; i<str.length(); i++){
+            answer += str.charAt(i) - 48;
+        }
+        return answer;
+    }
+}
