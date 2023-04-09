51922d0759c85f3dcf458876b1c463b80a87d090
blackpk1007 <34873340+blackpk1007@users.noreply.github.com>
2023-04-10 00:46:24 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        answer = new int[num2 - num1 + 1];
+        
+        for(int i=0; i<num2 - num1 + 1; i++){
+            answer[i] = numbers[num1+i];
+        }
+        return answer;
+    }
+}
