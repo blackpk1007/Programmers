726a3c05fd91528853878f32258a6056545b0945
blackpk1007 <34873340+blackpk1007@users.noreply.github.com>
2023-04-16 22:07:53 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -4,6 +4,7 @@ class Solution {
         answer = new int[numbers.length];
         for(int i=0; i<numbers.length; i++){
             answer[i] = numbers[i] * 2;
+            
         }
         return answer;
     }
