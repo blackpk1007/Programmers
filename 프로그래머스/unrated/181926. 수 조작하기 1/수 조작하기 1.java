a9d804edb58d360e6155070caeb40f157b4de7ed
Jeon Young Su <34873340+blackpk1007@users.noreply.github.com>
2023-05-09 22:07:50 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.00 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        for(int i=0; i<control.length(); i++){
+            if(control.charAt(i) == 'w'){
+                n += 1;
+            }else if(control.charAt(i) == 's'){
+                n -= 1;
+            }else if(control.charAt(i) == 'd'){
+                n += 10;
+            }else{
+                n -= 10;
+            }
+        }
+        answer = n;
+        return answer;
+    }
+}
