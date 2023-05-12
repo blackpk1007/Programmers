d6ada1b4779c8010fbcb105f6f97b0b385b5dbb8
Jeon Young Su <34873340+blackpk1007@users.noreply.github.com>
2023-05-12 22:07:42 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a % 2 == 1 && b % 2 == 1){
+            answer = (a*a + b*b);
+        }else if(a % 2 == 1 || b % 2 == 1){
+            answer = 2 * (a + b);
+        }else{
+            if(a < b){
+                answer = b - a;    
+            }else{
+                answer = a - b;
+            }
+            
+        }
+        return answer;
+    }
+}
