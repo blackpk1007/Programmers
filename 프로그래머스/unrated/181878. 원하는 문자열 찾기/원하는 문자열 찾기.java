7535050f158a3e3ca2f1645399405dfafd7a696b
Jeon Young Su <34873340+blackpk1007@users.noreply.github.com>
2023-05-13 20:07:17 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 5.42 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        
+        if(myString.contains(pat)){
+            answer = 1;
+        }
+        return answer;
+    }
+}
