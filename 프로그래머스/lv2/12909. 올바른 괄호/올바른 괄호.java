5b8acb1f2a3d824415c3adf6d81caff63909878a
Jeon Young Su <34873340+blackpk1007@users.noreply.github.com>
2023-05-19 02:07:04 +0900

    [level 2] Title: 올바른 괄호, Time: 15.58 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -6,16 +6,22 @@ class Solution {
         boolean answer = true;
         
        	Queue<Character> queue = new LinkedList<Character>();
+        // 문자열 시작이 ) 이면은 false
 		if(s.charAt(0) == ')') {
 			answer = false;
 		}
 		else {
+            // 문자열을 하나씩 쪼개서 배열로만들기
 			for(char c : s.toCharArray()) {
+                // 
 				if(c == '(') {
 					queue.add(c);
 				}else {
 					if(!queue.isEmpty()) {
 						queue.poll();
+					}else {
+						answer = false;
+						break;
 					}
 				}
 			}
